e472ef3bbbfb433e9c09c072040826dd7dcb012b
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-11-22 22:38:15 -0500

    desarrollando banner principal

--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -5,9 +5,33 @@ import styled from '@emotion/styled';
 
 const ImageBackground = styled(BackgroundImage)`
     height: 700px;
+`;
 
+const TextoImagen = styled.div`
+    background-image: linear-gradient(to top, rgba(34, 49, 63, .8), rgba(34, 49, 63, .8));
+    color: #fff;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    align-items: center; 
 
+    h1 {
+        font-size: 4rem;
+        margin: 0;
+    
+        @media(min-width: 992px) {
+            font-size: 5.8rem;
+        }
+    }
 
+    p { 
+        font-size: 2rem;
+        @media(min-width: 992px) {
+            font-size: 2.6rem;
+        
+    }
 `;
 
 const ImagenHotel = () => {
@@ -25,9 +49,14 @@ const ImagenHotel = () => {
     `);
 
     return (
-        <ImageBackground tag="section" fluid={image.sharp.fluid}
-        fadeIn="soft"
-        >
+        <ImageBackground
+            tag="section"
+            fluid={image.sharp.fluid}
+            fadeIn="soft">
+            <TextoImagen>
+                <h1>Bienvenido al Hotel Gatsby</h1>
+                <p>El mejor lugar para tus vacaciones</p>
+            </TextoImagen>
         </ImageBackground>
     )
 }
