ce93266f6145a1312be1c5af4ff41fc070a0c1ff
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-11-24 22:50:28 -0500

    gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,27 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const { graphql } = require("gatsby")
 
-// You can delete this file if you're not using it
+exports.createPages = async({ actions, grapql, reporter}) => {
+    const resultado = await graphql(`
+    query {
+        allDatoCmsHabitacion {
+            nodes {
+                slug
+            }
+        }
+    }
+    `);
+
+    if(resultado.errors) {
+        reporter.panic('No hubo resultados', resultado.errors);
+    }
+
+    // Crear los archivos si existen las paginas
+
+    const habitaciones = resultado.data.allDatoCmsHabitacion.nodes;
+
+    habitaciones.forEach(habitacion => {
+        actions.createPage({
+            
+        });
+    });
+}
