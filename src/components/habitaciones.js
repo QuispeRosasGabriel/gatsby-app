b00964db509d06253c1418dd42f2c1d88c89e1c4
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-11-25 21:47:04 -0500

    enviando query variable

--- src/components/habitaciones.js
+++ src/components/habitaciones.js
@@ -1,6 +1,25 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 
-const HabitacionTemplate  = () => {
+export const query = graphql`
+	query($slug: String!){
+  allDatoCmsHabitacion(filter: {slug: {eq: $slug}}) {
+    nodes {
+      titulo
+      id
+      slug
+      contenido
+      imagen {
+        fluid(maxWidth: 1200) {
+          ...GatsbyDatoCmsFluid
+        }
+      }
+    }
+  }
+}
+`;
+
+const HabitacionTemplate = ({data}) => {
     return (
         <div>
             Habitacion.js
