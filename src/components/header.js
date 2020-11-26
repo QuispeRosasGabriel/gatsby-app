bedc5a34f9b1f73bac46aded280b17efec413c7c
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-11-25 22:04:31 -0500

    terminando habitaciones

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import { css } from "@emotion/core";
 import Navegacion from './nav';
+import styled from '@emotion/styled';
+import { Link } from "gatsby";
+
+const EnlaceHome = styled(Link)`
+    color: #fff;
+    text-align: center;
+    text-decoration: none;
+`;
 
 const Header = () => {
     return (
         <header
-        css={css`
+            css={css`
             background-color: #222;
             padding: 1rem;
         `}
         >
             <div
-            css={css`
+                css={css`
             max-width: 1200px;
             margin: 0 auto;
 
@@ -22,14 +30,9 @@ const Header = () => {
             } 
             `}
             >
-                <h1
-                css={css`
-                    color: #fff;
-                    text-align: center;
-                `}
-                >Hotel Gatsby</h1>
+                <EnlaceHome to='/'>Hotel Gatsby</EnlaceHome>
                 <Navegacion />
-            </div>
+            </div> 
         </header>
     )
 }
